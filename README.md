e73f306924f6fbaf2d4539e6a857ac461ab17102
Charlie Lake <charlie@lakewest.ca>
2023-01-20 16:27:47 -0800

    Committing source code.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # cpsc121-prelab-script
 Prelab script to parse student grades from given Gradescope exported sheet and list of students.
 
-Used for the teaching team for UBC's CPSC 121 course. Note that this is simply my version of the project - an internal version is released on our CPSC 121 git that will be passed forward. However, this is the original version of the script I made. A script was originally created by JR (thank you!) for the teaching team - we lost track of it over time and created a new script inspired by JR's original script. 
+Used for the teaching team for UBC's CPSC 121 course. Note that this is simply my version of the project - an internal version is released on our CPSC 121 git that will be passed forward. 
+
+This is the original version of the script made my JR (thank you!), edited to fit the course's evolving needs.
 
 Files are organized as follows:
-* 
-*
-*
+* parser.js is the code that parses the 
+* style.css handles the style of the page
+* "CPSC 121 Pre-Lab Grades.html" has the page's HTML layout and interactible elements.
---
+++ parser.js
@@ -0,0 +1,81 @@
+const GRADESCOPE_HEADERS = {
+	Id: 'Name',
+	TotalScore: 'Total Score',
+	Status: 'Status'
+}
+
+let preLabGradesMap;
+
+function submitForm() {
+	// reset global vars
+	preLabGradesMap = new Map();
+
+	// reset warning section
+	document.getElementById('warning').style.display = 'none';
+	document.getElementById('warning-text').innerHTML = '';
+	
+	// get prelab file from file selector
+	let prelabFile = document.getElementById('gradescope-csv').files[0];
+	
+	// check if file exists
+	if (!prelabFile) {
+		document.getElementById('warning').style.display = 'block';
+		document.getElementById('warning-text').innerHTML = 'No pre-lab grades file was uploaded!<br>';
+		outputResult();
+		return;
+	}
+
+	// parse prelab grades file using PapaParse
+	// on callback: filter & map data to ids
+	Papa.parse(prelabFile, {
+		header: true,
+		complete: (results) => {
+			mapPrelabGradeData(results.data);
+		}
+	});
+}
+
+function mapPrelabGradeData(gradesArray) {
+	// populate map with [ID, prelab grade] pairs if it's graded
+	gradesArray.forEach((row) => {
+		if (row[GRADESCOPE_HEADERS.Status] == 'Graded') {
+			preLabGradesMap.set(row[GRADESCOPE_HEADERS.Id], row[GRADESCOPE_HEADERS.TotalScore]);
+		}
+	});
+	outputResult();
+}
+
+function outputResult() {
+	// get the ID list from the text area
+	// filter() removes any empty values that might have been added by additional new lines
+	let idArray = document.getElementById('id-list').value.split("\n").filter(el => el);
+
+	// get reference of results table
+	let table = document.getElementById('result');
+	
+	// delete any rows (except header row) that already exist
+	let numRows = table.rows.length;
+	for (let i = 0; i < numRows - 1; i++) {
+		document.getElementById('result').deleteRow(-1);
+	}
+
+	// set up inner tables (this is to support selecting/copying a single column of the table)
+	let idTable = document.createElement('table');
+	let gradeTable = document.createElement('table');
+	let row = table.insertRow(-1);
+	row.insertCell(-1).append(idTable);
+	row.insertCell(-1).append(gradeTable);
+
+	idArray.forEach((id) => {
+		// insert cells with id to new row in idTable
+		idTable.insertRow(-1).insertCell(-1).innerHTML = id;
+
+		// insert cell with grade to new row in gradeTable
+		if (preLabGradesMap.has(id)) {
+			gradeTable.insertRow(-1).insertCell(-1).innerHTML = preLabGradesMap.get(id);
+		} else {
+			gradeTable.insertRow(-1).insertCell(-1).innerHTML = '---';
+		}
+	});
+}
+
